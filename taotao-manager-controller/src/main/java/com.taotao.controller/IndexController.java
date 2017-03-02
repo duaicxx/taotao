4cb0fdc9d9348fbd6a6af1d205e47d0f6f200ab7
duai <1582293004@qq.com>
2017-03-02 11:31:33 +0800

    淘淘 启动成功

--- taotao-manager-controller/src/main/java/com.taotao.controller/IndexController.java
+++ taotao-manager-controller/src/main/java/com.taotao.controller/IndexController.java
@@ -6,6 +6,7 @@ import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by duai on 2017/2/28.
@@ -16,8 +17,9 @@ public class IndexController {
     @Autowired
     private ItemService itemService;
     @RequestMapping("index")
+    @ResponseBody
     public String index(long id) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
-        return mapper.writeValueAsString(itemService.getItemById(id));
+       return mapper.writeValueAsString(itemService.getItemById(id));
     }
 }
